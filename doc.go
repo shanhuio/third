9e4d2e53edc5514538b69a94da1f30dc9e2888e1
Lonnie Liu <liulonnie@gmail.com>
2017-07-30 08:17:39 -0700

    .

--- doc.go
+++ doc.go
@@ -3,4 +3,3 @@
 // The "shanhu.io/third" repository contains third party code that have
 // been modified to fit the 300-line no-circular dependency rule.
 package third // import "shanhu.io/third"
-
